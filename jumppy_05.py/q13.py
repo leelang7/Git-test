20fdef36c8746037e07f223eebc1bc78b82ed89a
DIDDUMON <jisooya5959@gmail.com>
2025-03-06 17:42:49 +0900

    Update q13.py

--- jumppy_05.py/q13.py
+++ jumppy_05.py/q13.py
@@ -2,6 +2,11 @@
 Q13. random 모듈을 이용해서 로또번호(1~45사이의 숫자 6개)를 생성해보자. (중복 숫자 안 됨)
 '''
 import random
-a=random.sample(range(1,46),6)
+a=[]  #번호 저장할 리스트
+
+while len(a)<6:
+  num.random.randint(1,45)
+  if num not in a:
+    a.append(num)
 
 print(a)
